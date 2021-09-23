2f7e4d8c73827617f58f8be1f3d5660677171ef3
jeremy <vivelerockpoint0@hotmail.fr>
2021-09-23 11:41:33 +0200

    start of exo 2

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-todo',
@@ -8,6 +10,13 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
 })
 export class TodoComponent {
   formArray = new FormArray([])
+  done$?: Observable<any>
+
+  ngOnInit() {
+    this.done$ = this.formArray.valueChanges.pipe(
+      map((value: ))
+    )
+  }
 
   add() {
     const todo = new FormGroup({
